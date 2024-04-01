361865bc8e8a0cc55704037af73103ff570b7edd
santiago_chaves <161728457+RommelAgudelo@users.noreply.github.com>
2024-04-01 10:07:30 -0500

    Logo y controlador y vista
    se cambio el logo en todas las pantallas y en el header

---
+++ Vista/js/controlador.js
@@ -0,0 +1,39 @@
+let vista = new Vista();
+
+/*Funcion que muestra la oantalla principal apenas carga la pagina*/
+
+document.body.onload = function () {
+  vista.mostrarPlantilla("paginaPrincipal", "contenido");
+};
+
+function volverInicio() {
+  vista.limpiarContenido("contenido");
+  vista.mostrarPlantilla("paginaPrincipal", "contenido");
+}
+
+function mostrarPantallaBusqueda() {
+  vista.limpiarContenido("contenido");
+  vista.mostrarPlantilla("busqueda", "contenido");
+}
+
+/* Funcion que identifica cuando se presiona ENTER en el input de busqueda */
+
+function handleKeyPress(event) {
+  if (event.key === "Enter") {
+    mostrarBusquedaRelacionada();
+  }
+}
+
+/* mostrar plantilla busqueda relacionada */
+
+function mostrarBusquedaRelacionada() {
+  vista.limpiarContenido("contenido");
+  vista.mostrarPlantilla("busquedaRelacionada", "contenido");
+}
+
+function mostrarModal() {
+  vista.abrirModal();
+}
+function cerrarModal() {
+  vista.cerrarModal();
+}
